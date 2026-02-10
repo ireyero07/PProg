22253f08d56ac4621b734d7b01ea96977b868738
Jian2319 <chen.yin.jian319@gmail.com>
2026-02-10 17:46:14 +0100

    añadi el d=drop t=take y cambie el limite de comandos a 7

--- seed-src_2026/command.h
+++ seed-src_2026/command.h
@@ -1,71 +1,105 @@
 /**
- * @brief It defines the command interpreter interface
+ * @brief It implements the command interpreter
  *
- * @file command.h
- * @author Profesores PPROG && Jian Feng Yin Chen
+ * @file command.c
+ * @author Profesores PPROG
  * @version 0
- * @date 02-02-2026
+ * @date 24-01-2026
  * @copyright GNU Public License
  */
 
-#ifndef COMMAND_H
-#define COMMAND_H
+#include "command.h"
 
-#include "types.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 
-#define N_CMDT 2
-#define N_CMD 5
+#define CMD_LENGHT 30
 
-typedef enum { CMDS, CMDL } CommandType;
-
-typedef enum { NO_CMD = -1, UNKNOWN, EXIT, NEXT, BACK } CommandCode;
-
-typedef struct _Command Command;
+char *cmd_to_str[N_CMD][N_CMDT] = {{"", "No command"}, {"", "Unknown"}, {"e", "Exit"}, {"n", "Next"}, {"b", "Back"},{"t", "Take"},{"d", "Drop"}};
 
 /**
- * @brief It creates a new command, allocating memory and initializing with code NO_CMD.
- * @author Jian Feng Yin Chen
- * 
- * @return A pointer to the new command if everything goes well, or NULL if memory could not be reserved.
- */
-Command* command_create();
-
-/**
- * @brief It destroys a command, freeing the allocated memory.
- * @author Jian Feng Yin Chen
- * @param command Pointer to the command that must be destroyed.
- * @return OK if everything goes well, or ERROR if there was some mistake.
- */
-Status command_destroy(Command* command);
-
-/**
- * @brief Sets the code of a command.
- * @author Jian Feng Yin Chen
+ * @brief Command
  *
- * @param command Pointer to the command to be modified.
- * @param code New value for the command code.
- * @return OK if everything goes well, or ERROR if the pointer is NULL.
+ * This struct stores all the information related to a command.
  */
-Status command_set_code(Command* command, CommandCode code);
+struct _Command {
+  CommandCode code; /*!< Name of the command */
+};
 
-/**
- * @brief Gets the code stored in a command.
- * @author Jian Feng Yin Chen
- *
- * @param command Pointer to the command from wich to obtain the code.
- * @return The command code stored in the structure, or NO_CMD if the pointer is NULL.
+/** space_create allocates memory for a new space
+ *  and initializes its members
  */
-CommandCode command_get_code(Command* command);
+Command* command_create() {
+  Command* newCommand = NULL;
 
-/**
- * @brief Reads user input and updates the command code accordingly.
- * @author Jian Feng Yin Chen
- * 
- * This function reads a line from stdin, parses the first word and compares it with the known commands to set the corresponding CommandCode.
- * @param command  Pointer to the command that will store the interpreted code.
- * @return OK if everything goes well, or ERROR if the pointer is NULL.
- */
-Status command_get_user_input(Command* command);
+  newCommand = (Command*)calloc(1,sizeof(Command));
+  if (newCommand == NULL) {
+    return NULL;
+  }
 
-#endif
+  /* Initialization of an empty command*/
+  newCommand->code = NO_CMD;
+
+  return newCommand;
+}
+
+Status command_destroy(Command* command) {
+  if (!command) {
+    return ERROR;
+  }
+
+  free(command);
+  command = NULL;
+  return OK;
+}
+
+Status command_set_code(Command* command, CommandCode code) {
+  if (!command) {
+    return ERROR;
+  }
+
+  command->code=code;
+
+  return OK;
+}
+
+CommandCode command_get_code(Command* command) {
+  if (!command) {
+    return NO_CMD;
+  }
+  return command->code;
+}
+
+Status command_get_user_input(Command* command) {
+  char input[CMD_LENGHT] = "", *token = NULL;
+  int i = UNKNOWN - NO_CMD + 1;
+  CommandCode cmd;
+
+  if (!command) {
+    return ERROR;
+  }
+
+  if (fgets(input, CMD_LENGHT, stdin)) {
+    token = strtok(input, " \n");
+    if (!token) {
+      return command_set_code(command, UNKNOWN);
+    }
+
+    cmd = UNKNOWN;
+    while (cmd == UNKNOWN && i < N_CMD) {
+      if (!strcasecmp(token, cmd_to_str[i][CMDS]) || !strcasecmp(token, cmd_to_str[i][CMDL])) {
+        cmd = i + NO_CMD;
+      } else {
+        i++;
+      }
+    }
+    return command_set_code(command, cmd);
+  }
+  else
+    return command_set_code(command, EXIT);
+  
+}
+
 
